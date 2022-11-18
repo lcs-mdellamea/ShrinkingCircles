24266fda4f6b052f978f950d07088080900799bc
Madison Dellamea <mdellamea@lcs.on.ca>
2022-11-18 15:16:58 -0500

    Set up the recursiveHelper function to draw the circles.

--- ShrinkingCircles/ContentView.swift
+++ ShrinkingCircles/ContentView.swift
@@ -7,29 +7,60 @@
 
 import SwiftUI
 //"Shape" protocol
-struct ShrinkingCircles: Shape {
+//struct ShrinkingCircles: Shape {
+//
+//    func path(in rect: CGRect) -> Path {
+//        //At start of function, return a path
+//        var i = 0...8 {
+//
+//            let j = CGFloat(i)
+//
+//            path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.midY + 25 * j, y: 0 + 25 * j),
+//                                       size: CGSize(width: rect.height - 50 * j,
+//                                                    height: rect.height - 50 * j)))
+//
+//        }
+//
+//        // At end of function, return path
+//        return path
+//
+//    }
+//}
+
+struct ShrinkingCirclesRecursively: Shape {
+    
+    // MARK: Stored Properties
+    let desiredDepth: Int
+    
+    // MARK: Functions
     
     func path(in rect: CGRect) -> Path {
-        //At start of function, return a path
-        var i = 0...8 {
-            
-            let j = CGFloat(i)
-            
-            path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.midY + 25 * j, y: 0 + 25 * j),
-                                       size: CGSize(width: rect.height - 50 * j,
-                                                    height: rect.height - 50 * j)))
-            
-        }
         
-        // At end of function, return path
+        var path = Path()
+        
+        
+        // INVOKE: Beging calling the recursive helper
+        let allThePaths = recursiveHelper()(currentDepth: 1)
+        path.addPath(allThePaths)
+        
         return path
         
+    }
+    func recursiveHelper() -> Path {
+        // Make the path
+        var path = Path()
+        
+        // return the path
+        return path
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        ShrinkingCircles()
+//        ShrinkingCircles()
+//            .stroke()
+        
+        ShrinkingCirclesRecursively(desiredDepth: 4)
             .stroke()
     }
 }
