f937d670940682a8aec8572e19b3281bbb8d432d
Madison Dellamea <mdellamea@lcs.on.ca>
2022-11-18 14:21:23 -0500

    Set up a blank shape and we are showing on canvas. Made loop better.

--- ShrinkingCircles/ContentView.swift
+++ ShrinkingCircles/ContentView.swift
@@ -6,16 +6,31 @@
 //
 
 import SwiftUI
+//"Shape" protocol
+struct ShrinkingCircles: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        //At start of function, return a path
+        var path = Path()
+        
+        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX + 25 * 0, y: 0), size: CGSize(width: rect.height - 50 * 0, height: rect.height - 50 * 0)))
+        
+        path.addEllipse(in: CGRect(origin: CGPoint(x: (rect.midX - rect.midY) + 25, y: 0 + 25), size: CGSize(width: rect.height - 50 * 1, height: rect.height - 50 * 1)))
+        
+        path.addEllipse(in: CGRect(origin: CGPoint(x: (rect.midX - rect.midY) + 25, y: 0 + 25), size: CGSize(width: rect.height - 50 * 2, height: rect.height - 50 * 2)))
+        
+        path.addEllipse(in: CGRect(origin: CGPoint(x: (rect.midX - rect.midY) + 25, y: 0 + 25), size: CGSize(width: rect.height - 50 * 3, height: rect.height - 50 * 3)))
+        
+        // At end of function, return path
+        return path
+        
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ShrinkingCircles()
+            .stroke()
     }
 }
 
