37d01d5e5b92ecc4d640205573f55d29002f4034
Madison Dellamea <mdellamea@lcs.on.ca>
2022-11-18 14:17:33 -0500

    Initial Commit

---
+++ ShrinkingCircles/ShrinkingCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShrinkingCirclesApp.swift
+//  ShrinkingCircles
+//
+//  Created by Madison Dellamea on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct ShrinkingCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
